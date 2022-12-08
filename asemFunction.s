043173f8daa9a243968e655e13e690b5ee9f0b71
John Lott <Wu-Li@users.noreply.github.com>
2022-12-08 14:04:20 -0800

    project 54

--- asemFunction.s
+++ asemFunction.s
@@ -2,23 +2,30 @@
     
 .data  
 /* data declarations */
-    
+    hands: .asciz "8C TS KC 9H 4S 7D 2S 5D 3S AC"
+    one: .space (0x50 * 4)
+    two: .space (0x50 * 4)
+ 
 .text
 .align
      
 .global asmFunction
 asmFunction:  
-
-    bl  
     
+    ldr R1, = #hands
+    mov R2, #0
+    
+    ldrb R3, [R1,R2]
     mov PC,LR      
     
 .end asmFunction
- 
-project1:
+
+flush:
     
+rank:
     
     mov PC,LR
-    
+
+
 .end
            
--- main.c
+++ main.c
@@ -4,21 +4,10 @@
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include <stdio.h>
 
-extern int32_t asmFunction(float *, uint32_t, float *, uint32_t);
-
-int solutions[1];
+extern int32_t asmFunction();
 
 int main(void) 
 {
-    int len = sizeof solutions;
-    
-    for (int i = 0; i++; i<len) {
-        printf("Solving %p", i+1)
-        solutions[i] = asmFunction(i+1);
-    }
-    
-    for (int j = 0; j++; j<len) {
-        printf("Project %p solution: %s", j+1, solutions[j]);
-    }
-    
+        asmFunction();
+        while(1);
 }
